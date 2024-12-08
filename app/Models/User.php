cdf16157cb3c755dc5c5e93529f3312b836cd904
Orelson14 <orelson14@gmail.com>
2024-12-08 15:00:49 +0100

    eleventh commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,16 @@
 <?php
-
 namespace App\Models;
-
+ 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+ 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+ 
     /**
      * The attributes that are mass assignable.
      *
@@ -22,7 +21,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
+ 
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -32,7 +31,7 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
+ 
     /**
      * Get the attributes that should be cast.
      *
@@ -44,5 +43,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+ 
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
     }
 }
