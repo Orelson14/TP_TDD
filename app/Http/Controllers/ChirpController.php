f65a96509a3a40a827a419118c84f34f246a9d42
Orelson14 <orelson14@gmail.com>
2024-12-08 14:39:53 +0100

    seventh commit

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use Illuminate\Http\Request; 
 use Illuminate\Http\Response; 
+use Illuminate\View\View;
  
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response 
+    public function index(): View
     {
-        return response('Hello, World!');
+        return view('chirps.index');
     }
     /**
      * Show the form for creating a new resource.
